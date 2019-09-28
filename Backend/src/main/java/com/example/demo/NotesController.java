78bcf71aca736b92635471de242eea285b07a565
Gokul <gokulramalingam1@gmail.com>
2019-09-28 22:06:42 +0530

    update in files

---
+++ Backend/src/main/java/com/example/demo/NotesController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+@RestController
+public class NotesController {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping("/notes/get")
+	public List<Notes> notesList(){
+		List<Notes> noteItems = jdbcTemplate.query("SELECT * FROM LIST",
+			(rs,rowNum)->
+        new Notes(
+        		rs.getInt("id1"),
+        		rs.getString("datas"),
+        		rs.getBoolean("isDone")
+      		 ));
+		return noteItems;
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping("/notes/patch")
+	public void updateList(@RequestParam("id1") int id1,@RequestParam("datas") String datas){
+		jdbcTemplate.update("UPDATE LIST SET datas=? WHERE id1=?","working 9",3);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/notes/post")
+	public void insertIntoList(@RequestParam("id1") int id1,@RequestParam("datas") String datas,@RequestParam("isDone") boolean isDone){
+		System.out.println("here");
+		jdbcTemplate.update("INSERT INTO LIST VALUES(?,?,?)",id1,datas,isDone);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@DeleteMapping(path="/notes/delete/{id}")
+	public ResponseEntity<Void> deleteCustomer(@PathVariable int id){
+		jdbcTemplate.update("DELETE FROM LIST WHERE id1 = ?",id);
+		
+		return ResponseEntity.noContent().build();
+	          
+	  }
+}
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Notes } from './Notes'
-import { HttpClient, HttpErrorResponse} from '@angular/common/http'
-import { Headers, Http } from '@angular/http';
+import { HttpClient, HttpErrorResponse , HttpHeaders} from '@angular/common/http'
+import { Headers, Http, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw'
@@ -12,7 +12,7 @@ export class DataService {
   public id = 4;
   public deleteId;
   public baseUrl = 'http://localhost:8080/notes'
-  private headers = new Headers({'Content-Type': 'application/json'});
+  
 
   constructor(private http:Http , private http1:HttpClient) { }
 
@@ -30,21 +30,20 @@ changeData(id1)
   // return this.http.delete('http://localhost:8080/weather/delete?id1='+id1)  
   //     .catch(this.errorHandler);   
 
-      const url = `${this.baseUrl}/delete/1`;
-    return this.http.delete(url, {headers:this.headers})
+      const url = `${this.baseUrl}/delete/`+id1;
+      return this.http.delete(url, {headers:this.headers})
       .toPromise()
       .then(() => null)
       .catch(this.errorHandler);
 }
 
-addNotes(datas,id1,isDone)
+ addNotes(datas,id1,isDone)
   {
-   
    this.id += id1;
     id1 = this.id;
-    console.log(id1) 
-   this.notes.push({id1,datas,isDone})
-   this.http.post('http://localhost:8080/notes/post',{"id1":id1,"datas":datas,"isDone":isDone})
+    let json = JSON.stringify({"id1":id1,"datas":datas,"isDone":isDone});
+        let params = 'json=' + json;
+  return this.http.post(`${this.baseUrl}/post`, params);
   }
 
 }
